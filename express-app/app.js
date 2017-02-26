1698672e7f8602a7f7d87dba349893253477f47f
Raul Mar <raulmar_94@hotmail.com>
2017-02-26 03:08:12 -0600

    Login implemented, only missing tests....

--- express-app/app.js
+++ express-app/app.js
@@ -1,9 +1,9 @@
 const express = require('express'),
     app = express(),
     mainRoutes = require('./routes/index.js'),
-    userRoutes = require('./routes/user.js'),
     path = require('path'),
     User = require('./models/user.js')
+    cors = require('cors')
 
 //Database connection
 const connect = require('camo').connect;
@@ -19,6 +19,8 @@ connect(uri).then(function(db) {
 //template engine
 const hbs = require('hbs');
 hbs.registerPartials(path.join(__dirname + '/views/partials'));
+// setup cors
+app.use(cors())
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -34,9 +36,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //routes
 app.use('/', mainRoutes);
-
-app.use('/user', userRoutes);
-
 
 
 app.listen(3000, function () {
--- express-app/public/js/renderer.js
+++ express-app/public/js/renderer.js
@@ -1,3 +1,4 @@
+//template js, we need??
 $(document).ready(function(){
 
 
@@ -16,45 +17,17 @@ $(document).ready(function(){
       $li.addClass("open");
     }
   });
-  
+
+
+
 });
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-// onload = () => {
-
-// 	const webview = document.getElementById('foo')
-
-// 	webview.addEventListener('dom-ready', () => {
-// 	 	webview.openDevTools()
-// 		alert(webview.document.getElementById('#estadoDesde'))
-// 	})
-// }
 
 
-// function lePico() {
-// 	$.get("http://swapi.co/api/films/1/", function(data) {
-// 		console.log(data)
-// 	})
-// }
+// function to tell if online
+function isOnline() {
+	if (navigator.onLine) return true
+	else return false
+}
 
-// const {ipcRenderer} = require('electron')
-// const electron = require('electron')
-// // Module to control application life.
-// const app = electron.app
-// const updateOnlineStatus = () => {
-// 	ipcRenderer.send('online-status-changed', navigator.onLine ? 'online' : 'offline')
-// }
+// isOnline()
 
-// window.addEventListener('online',  updateOnlineStatus)
-// window.addEventListener('offline',  updateOnlineStatus)
-
-// updateOnlineStatus()
-
-// function lepico() {
-// 	var content = document.getElementById('first_name');
-// 	alert(content)
-// 	console.log(content)
-// 	ipcRenderer.send('huehue', content)
-// }

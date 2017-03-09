6beb0146b8d91aebc1147b8b912a554de7a5c0df
adal347 <adalaidc@acm.org>
2017-03-08 18:55:56 -0600

    Requested Changes
    This commit resolves the request changes, I put the defult url.
    I delete the log out button. I find a better form to use launch the
    app without do a production. I create all possible test for log in.

--- test/login.test.js
+++ test/login.test.js
@@ -1,36 +1,69 @@
 var Application = require('spectron').Application;
+const path = require('path');
 var assert = require('assert');
 
+var electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
+
+if (process.platform === 'win32') {
+    electronPath += '.cmd';
+}
+
+var app = new Application({ path: electronPath });
+
 describe('application launch', function () {
-  this.timeout(10000);
+  this.timeout(20000);
 
   beforeEach(function () {
-    this.app = new Application({
-      path: '../jp2-win32-x64/jp2.exe'
-    });
-    return this.app.start();
+    return app.start();
   });
 
   afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
+    if (app && app.isRunning()) {
+      return app.stop();
     }
   });
 
   it('shows an initial window', function () {
-    return this.app.client.getWindowCount().then(function (count) {
-      assert.equal(count, 2);
+    return app.client.getWindowCount().then(function (count) {
+      assert.equal(count, 1);
     });
   });
 
   it('should see login form', function () {
-    var username = this.app.client.elementIdText('username');
-    var password = this.app.client.elementIdText('password');
-    var submit = this.app.client.element('//button/*[text(),Iniciar sesión]');
+    var username = app.client.elementIdText('username');
+    var password = app.client.elementIdText('password');
+    var submit = app.client.element('//button/*[text(),Iniciar sesión]');
+    username.keys('someusername');
+    password.keys('somepassword');
+
+    //click on signin button
+    submit.click()
+      .then(() => done());
+  });
+
+  it('should login successful', function () {
+    var username = app.client.elementIdText('username');
+    var password = app.client.elementIdText('password');
+    var submit = app.client.element('//button/*[text(),Iniciar sesión]');
     username.keys('someusername');
     password.keys('somepassword');
 
     //click on signin button
     submit.click();
+    app.client.waitForText('Hola someusername')
+      .then(() => done());
+  });
+
+  it('should login fail', function () {
+    var username = app.client.elementIdText('username');
+    var password = app.client.elementIdText('password');
+    var submit = app.client.element('//button/*[text(),Iniciar sesión]');
+    username.keys('someusername');
+    password.keys('somepasswordfail');
+
+    //click on signin button
+    submit.click();
+    app.client.waitForText('Iniciar sesión')
+      .then(() => done());
   });
 });

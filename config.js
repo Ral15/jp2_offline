f78d41222d116dee8e99afc98f97a2eef0d270f7
Raul Mar <raulmar_94@hotmail.com>
2017-04-08 17:46:44 -0600

    PR corrections..

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   //production
   ENV: 'testing',
   SECRET_SESSION: 'erikiano',
+  //test credentials
   username: 'eugenio',
   password: 'erikiado123',
   apiToken: '082be9526329ebea51d69e11a50217d93f21545d',

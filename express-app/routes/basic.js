39d33fc46c5987973dc9a5a15a73ff52d4190992
adal347 <adalaidc@acm.org>
2017-04-18 17:00:16 -0500

    Delete error page.

--- express-app/routes/basic.js
+++ express-app/routes/basic.js
@@ -8,10 +8,4 @@ router.get(urls.home, function(req, res) {
   res.render('login');
 });
 
-//GET error page.
-router.get(urls.error, function(req, res) {
-  res.render('error');
-});
-
-
 module.exports = router;
